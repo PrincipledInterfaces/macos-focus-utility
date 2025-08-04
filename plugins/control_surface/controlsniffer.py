2d881f5c623d29ebd4441b2bdc09a6db9b5338aa
Matthew Reichard <squid05@hotmail.com>
2025-08-04 13:44:32 -0500

    control surface plugin updated

--- plugins/control_surface/controlsniffer.py
+++ plugins/control_surface/controlsniffer.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python3
+
 import serial
 import serial.tools.list_ports
+
+#open queued mode file, overwrite as blank.
+
 
 def find_esp8266():
     ports = serial.tools.list_ports.comports()
@@ -17,8 +22,26 @@ if not port:
 
 ser = serial.Serial(port, 9600, timeout=1)
 
+
+def button_1_action():
+    pass
+
+def button_2_action():
+    pass
+
+def button_3_action():
+    pass
+
+
+#loop check for button signals
 print(f"Connected to {port}")
 while True:
     if ser.in_waiting:
         line = ser.readline().decode().strip()
         print(f"Received: {line}")
+        if line is "button1":
+            button_1_action()
+        elif line is "button2":
+            button_2_action()
+        elif line is "button3":
+            button_3_action()
